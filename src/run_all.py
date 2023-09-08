68570b22ab5a27cbe785effcdb7132852379dcec
smaharjan02 <smaharjan02@student.ysu.edu>
2023-09-08 13:45:10 -0400

    data_logs 10gb

---
+++ src/run_all.py
@@ -0,0 +1,17 @@
+import subprocess
+
+commands = [
+    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.5 -ls 10 -bs 200",
+    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.5 -ls 10 -bs 2000",
+    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.1 -ls 10 -bs 200",
+    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.1 -ls 10 -bs 2000",
+    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 1 -ls 10 -bs 200",
+    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 1 -ls 10 -bs 2000",
+    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.5 -ls 10 -bs 200",
+    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.5 -ls 10 -bs 2000",
+    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.1 -ls 10 -bs 200",
+    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.1 -ls 10 -bs 2000",
+]
+
+for cmd in commands:
+    subprocess.run(cmd, shell=True)
