d54a58925bb4398c3e4df23786a860a257008c44
smaharjan02 <smaharjan02@student.ysu.edu>
2023-09-19 02:27:46 -0400

    new graphs

--- src/run_all.py
+++ src/run_all.py
@@ -1,17 +1,22 @@
 import subprocess
 import time
 
+# commands = [
+#     "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.5 -ls 10 -bs 200",
+#     "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.5 -ls 10 -bs 2000",
+#     "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.1 -ls 10 -bs 200",
+#     "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.1 -ls 10 -bs 2000",
+#     "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 1 -ls 10 -bs 200",
+#     "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 1 -ls 10 -bs 2000",
+#     "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.5 -ls 10 -bs 200",
+#     "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.5 -ls 10 -bs 2000",
+#     "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.1 -ls 10 -bs 200",
+#     "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.1 -ls 10 -bs 2000",
+# ]
 commands = [
-    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.5 -ls 10 -bs 200",
-    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.5 -ls 10 -bs 2000",
-    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.1 -ls 10 -bs 200",
-    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 0.1 -ls 10 -bs 2000",
-    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 1 -ls 10 -bs 200",
-    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 1 -ls 10 -bs 2000",
-    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.5 -ls 10 -bs 200",
-    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.5 -ls 10 -bs 2000",
-    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.1 -ls 10 -bs 200",
-    "python3 driver.py -df tpch_10g.db -qr query_fourth_join.sql -sr 0.1 -ls 10 -bs 2000",
+    "python3 driver.py -df tpch_10g.db -qr query_first_join.sql -sr 1.0 -ls 10 -bs 2000",
+    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 1.0 -ls 10 -bs 200",
+    "python3 driver.py -df tpch_10g.db -qr query_third_join.sql -sr 1.0 -ls 10 -bs 2000",
 ]
 
 for cmd in commands:
